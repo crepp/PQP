d688f4ea7e23a6a045eb48f6e840eb183556753b
Yvoz <yvoz.lg@gmail.com>
2019-03-19 09:12:54 +0100

    Update pqp_config.py

--- pqp_config.py
+++ pqp_config.py
@@ -2,17 +2,19 @@
 # GPL V3.0
 # www.crepp.org - yvoz.lg@gmail.com
 
-# Durée du Wifi lors d'une mise sous tension (hard reset)
+# Durée du Wifi actif lors d'une mise sous tension (hard reset)
 # wifitime_hardreset = 3mn minimum 
 wifitime_hardreset = 3*60*1000
 
-# Durée du Wifi apres un reveil par l'alarme (deepsleep reset)
+# Durée du Wifi actif apres un reveil par l'alarme (deepsleep reset)
 # Cette durée doit être supérieure à la durée totale de la tâche courante (l'arrosage)
 # wifitime = 15 secondes
 wifitime = 15*1000 
 
-# Durée de la mise en veille - 3 options possibles
-# sleeptime = -1: pas de mise en veille / 0: deepsleep jusqu'à la prochaine heure / 30*60*1000: 30mn de deepsleep
+# Durée de la mise en veille - 3 options possibles :
+# -1 : pas de mise en veille 
+#  0 : sleeptime jusqu'à la prochaine heure fixe 
+#  30*60*1000 : 30mn de mise en veille
 sleeptime = 0
 
 # Durée de la mise en action de la pompe - tenir compte du temps d'amorçage / la hauteur de pompage
